262b5f0710166383403def4c7a53c52d6f523dda
Vidyarathna. B <vidyarb30@gmail.com>
2024-06-15 16:18:09 +0530

    Create solution.py

---
+++ solve-me-first/solution.py
@@ -0,0 +1,8 @@
+def solveMeFirst(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    a = int(input().strip())
+    b = int(input().strip())
+    result = solveMeFirst(a, b)
+    print(result)
